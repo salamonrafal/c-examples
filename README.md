808116255c7d3333ecdab9ed97daba4fb9c250a8
Rafal Salamon <rasa@salamonrafal.pl>
2020-06-15 23:06:55 +0200

    Init template struct

---
+++ README.md
@@ -0,0 +1,3 @@
+# C-EXASMPLE
+
+This is template for Visual code with configured ```task.json``` for build C/C++ code.
---
+++ src/main.h
@@ -0,0 +1,9 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#include <iostream>
+
+using namespace std;
+
+int main(void);
+
+#endif
